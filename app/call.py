4aa4551d6094a1013c56c28c0227475997049534
DOOROOEDS-기업부설연구소1 <sskid12@naver.com>
2023-08-10 13:14:25 +0900

    case 추가

--- app/call.py
+++ app/call.py
@@ -5,7 +5,7 @@ url = "http://192.168.68.58:70"
 
 def select_case_1():
     a = requests.get(url=url+'/func_1', params={
-        "idx": "1"
+        "idx": "4"
     })
 
     return a.json()
@@ -16,4 +16,45 @@ def select_case_2():
 
     return a.json()
 
-print(select_case_2())
+
+def insert_case_1():
+    a = requests.post(url=url+'/func_2', params={
+        "input_1": "1",
+        "input_2": "2",
+        "create_date": "2023-08-10 13:08:00"
+    })
+    return a.content
+
+
+def insert_case_2():
+    a = requests.post(url=url+'/func_2', params={
+        "input_1": "1"
+    })
+    return a.json()
+
+
+def insert_case_3():
+    a = requests.post(url=url+'/func_2', params={
+        "input_2": "1"
+    })
+    return a.json()
+
+
+def update_case_1():
+    a = requests.post(url=url+'/func_1', params={
+        "idx": "4",
+        "input_1": "abcde",
+        "input_2": "bbbbb"
+    })
+    return a.json()
+
+
+def update_case_2():
+    a = requests.post(url=url+'/func_1', params={
+        "input_1": "aalskjda",
+        "input_2": "asdlkjas"
+    })
+    return a.json()
+
+
+print(update_case_1())
