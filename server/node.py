bc669699a222f1c6a3a33af6a812eac276fb13e0
송민규 <sskid12@naver.com>
2024-03-29 21:19:12 +0900

    - 기존 : main.py에서 Postman data를 가공해서 Query.py에 전달   - 전달 값의 [필수, 선택] 성격을 함수 인자로 부여 - 변경 : main.py에서 Postman data를 가공하지 않고 그대로 Query.py에 전달   - 필수 > Dict 내 key 값으로 value 호출   - 선택 > .get 기능을 통해 예외 처리

---
+++ server/node.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('cmd /k node postman_response_node_v1.4.0.js test')
+# os.system('\\postman_response_node-main\\server_run.bat')
+# print(subprocess.call([r'server_run.bat']))
---
+++ server/postman_response_node-main/server_run.bat
@@ -0,0 +1,2 @@
+
+cmd /k node postman_response_node_v1.4.0.js test
